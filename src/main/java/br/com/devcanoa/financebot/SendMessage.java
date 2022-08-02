5fdee414f699e88bf806ad6c6d0133fbdb441950
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-08-02 18:19:42 -0300

    remove info

--- src/main/java/br/com/devcanoa/financebot/SendMessage.java
+++ src/main/java/br/com/devcanoa/financebot/SendMessage.java
@@ -14,12 +14,13 @@ public class SendMessage {
 
     public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
     public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+    public static final String MY_NUMBER = System.getenv("MY_NUMBER");
 
     @GetMapping("/send")
     public ResponseEntity<String> send() {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         var message = Message.creator(
-                        new PhoneNumber("whatsapp:+559192933831"),
+                        new PhoneNumber("whatsapp:" + MY_NUMBER),
                         new PhoneNumber("whatsapp:+14155238886"),
                         "Consegui Enviar a Mensagem!!")
                 .create();
