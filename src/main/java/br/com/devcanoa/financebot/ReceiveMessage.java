fb84330e69d269d477c7b7acc0861d36aa9bd944
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-08-02 18:41:07 -0300

    add xml media type

--- src/main/java/br/com/devcanoa/financebot/ReceiveMessage.java
+++ src/main/java/br/com/devcanoa/financebot/ReceiveMessage.java
@@ -3,6 +3,7 @@ package br.com.devcanoa.financebot;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class ReceiveMessage {
 
-    @GetMapping("/receive")
+    @GetMapping(value = "/receive", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<String> receive() {
         var body = new Body.Builder("Consegui Te Responder!!").build();
         var message = new Message.Builder().body(body).build();
