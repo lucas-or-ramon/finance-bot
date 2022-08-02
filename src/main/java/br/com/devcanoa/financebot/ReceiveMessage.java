4d78694310ac7dfcd16aed14745f9e9949b6ab45
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-08-02 19:16:38 -0300

    add request to finance-api

--- src/main/java/br/com/devcanoa/financebot/ReceiveMessage.java
+++ src/main/java/br/com/devcanoa/financebot/ReceiveMessage.java
@@ -8,14 +8,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping
 public class ReceiveMessage {
 
+    public static final String FINANCE_API = System.getenv("FINANCE_API");
+
     @PostMapping(value = "/receive", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<String> receive() {
-        var body = new Body.Builder("Consegui Te Responder!!").build();
+
+        var response = new RestTemplate().getForEntity(FINANCE_API + "/resume/monthly/2022/08", String.class).getBody();
+
+        var body = new Body.Builder(response).build();
         var message = new Message.Builder().body(body).build();
         var messagingResponse = new MessagingResponse.Builder().message(message).build();
 
