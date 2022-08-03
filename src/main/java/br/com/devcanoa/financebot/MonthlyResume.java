c1a97ab1fc86fcf42e7be6a5f8338adbf2055fc0
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-08-03 11:32:44 -0300

    customizing whatsapp message

--- src/main/java/br/com/devcanoa/financebot/MonthlyResume.java
+++ src/main/java/br/com/devcanoa/financebot/MonthlyResume.java
@@ -1,15 +1,16 @@
 package br.com.devcanoa.financebot;
 
 import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.List;
+import java.util.Locale;
 
 public record MonthlyResume(LocalDate date, double balance, double totalRevenue, double totalExpenditure, List<CategoryResume> categoryResumeList) {
     @Override
     public String toString() {
-        return "Resumo Mensal de " + date.getMonth().name() + "\n"
-                + "Total de Entradas: " + totalRevenue + "\n"
-                + "Total de Saídas: " + totalExpenditure + "\n"
-                + "Saldo do Mês: " + balance;
-
+        return "Resumo Mensal de _" + date.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt")) + "/" + date.getYear() + "_\n"
+                + "*Total de Entradas*: R$ " + totalRevenue + "\n"
+                + "*Total de Saídas*: R$ " + totalExpenditure + "\n"
+                + "*Saldo do Mês*: R$ " + balance;
     }
 }
