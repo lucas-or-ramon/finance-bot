ba9192c523f8599b59f7524f0c04f7803a432552
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-08-06 15:06:07 -0300

    try get body

--- src/main/java/br/com/devcanoa/financebot/ReceiveMessage.java
+++ src/main/java/br/com/devcanoa/financebot/ReceiveMessage.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 @RestController
 @RequestMapping
@@ -26,7 +29,12 @@ public class ReceiveMessage {
     public ResponseEntity<String> receive(@RequestBody String request) throws IOException {
 
 //        var response = new RestTemplate().getForEntity(FINANCE_API + "/resume/annual/2022/08", AnnualResume.class).getBody();
-        var body = new Body.Builder(request).build();
+        var properties = new Properties();
+        properties.load(IOUtils.toInputStream(request, StandardCharsets.UTF_8));
+
+        var whatsappRequest = new WhatsappRequest(properties.getProperty("Body"), properties.getProperty("From"), properties.getProperty("ProfileName"));
+
+        var body = new Body.Builder(whatsappRequest.toString()).build();
         var message = new Message.Builder().body(body).build();
         var messagingResponse = new MessagingResponse.Builder().message(message).build();
 
