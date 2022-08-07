c85e31a89d3e0cb3b904551862b356bbd7357082
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-08-07 11:48:00 -0300

    remove space

--- src/main/java/br/com/devcanoa/financebot/AnnualResume.java
+++ src/main/java/br/com/devcanoa/financebot/AnnualResume.java
@@ -12,9 +12,9 @@ public record AnnualResume(double balance, double totalRevenue, double totalExpe
         return "\n\t" + bold(italic(monthlyResumeList.get(0).getMonthSlashYear()))
                 + " Até "
                 + bold(italic(monthlyResumeList.get(monthlyResumeList.size() - 1).getMonthSlashYear())) + "\n"
-                + bold(italic("Total de Entradas: ")) + spaceBefore(bold("R$ " + totalRevenue)) + "\n"
-                + bold(italic("Total de Saídas: ")) + spaceBefore(bold("R$ " + totalExpenditure)) + "\n"
-                + bold(italic("Saldo: ")) + spaceBefore(bold("R$ " + balance)) + "\n\n"
+                + bold(italic("Total de Entradas:")) + spaceBefore(bold("R$ " + totalRevenue)) + "\n"
+                + bold(italic("Total de Saídas:")) + spaceBefore(bold("R$ " + totalExpenditure)) + "\n"
+                + bold(italic("Saldo:")) + spaceBefore(bold("R$ " + balance)) + "\n\n"
                 + monthlyResumeList;
     }
 
